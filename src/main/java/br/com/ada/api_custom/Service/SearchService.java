738076e1b78c673ea577ea8b6bd88a7638225527
Ada <adacayres@outlook.com>
2021-04-05 15:01:46 -0300

    lombok log

--- src/main/java/br/com/ada/api_custom/Service/SearchService.java
+++ src/main/java/br/com/ada/api_custom/Service/SearchService.java
@@ -2,6 +2,8 @@ package br.com.ada.api_custom.Service;
 
 import br.com.ada.api_custom.Model.Search;
 import br.com.ada.api_custom.Repository.SearchRepository;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -12,29 +14,31 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class SearchService {
+
 
     @Autowired
     private SearchRepository searchRepository;
 
     public List<Search> listar() {
-        System.out.println("["+ System.nanoTime() +"]" + "Exhibiting all searches in our database");
+        log.info("["+ System.nanoTime() +"]" + "Exhibiting all searches in our database");
         return searchRepository.findAll();
     }
 
 
     public Search getToSearch(long id){
-        System.out.println("["+ System.nanoTime() +"]" + "Looking for search in our database");
+        log.info("["+ System.nanoTime() +"]" + "Looking for search in our database");
         Optional<Search> search = searchRepository.findById(id);
         if(!search.isPresent())throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Id not found");
-        System.out.println("["+ System.nanoTime() +"]" + "Found search in our database");
+        log.info("["+ System.nanoTime() +"]" + "Found search in our database");
         return search.get();
     }
 
 
     public Search adicionar(@RequestBody Search search) {
-        System.out.println("["+ System.nanoTime() +"]" + "Saving search in our database");
+        log.info("["+ System.nanoTime() +"]" + "Saving search in our database");
         return searchRepository.save(search);
     }
 
